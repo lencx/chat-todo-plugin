2c438e43ad0f8115d55798b0a6123915b93c7cdd
lencx <cxin1314@gmail.com>
2023-03-24 14:55:24 +0800

    add todo

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # chat-todo-plugin
+
+> ChatGPT Plugin for managing a TODO list
+
+- [ChatGPT plugins](https://openai.com/blog/chatgpt-plugins)
+
+## Usage
+
+```bash
+# install deps
+poetry install
+
+# run the service
+poetry run python main.py
+```
+
+## TODO
+
+### list
+
+```bash
+curl -X GET http://0.0.0.0:5002/todos \
+ -H 'Content-Type: application/json' \
+ -H 'Authorization: Bearer SERVICE_AUTH_KEY'
+```
+
+### add
+
+```bash
+curl -X POST http://0.0.0.0:5002/todos/lencx \
+ -H 'Content-Type: application/json' \
+ -H 'Authorization: Bearer SERVICE_AUTH_KEY' \
+ -d '{ "todo": "hello" }'
+```
+
+### delete
+
+```bash
+curl -X DELETE http://0.0.0.0:5002/todos/lencx \
+ -H 'Content-Type: application/json' \
+ -H 'Authorization: Bearer SERVICE_AUTH_KEY' \
+ -d '{ "todo_idx": 0 }'
+```
---
+++ main.py
@@ -0,0 +1,82 @@
+import json
+import os
+import quart
+import quart_cors
+from quart import Quart, jsonify, request
+
+# Create a Quart app and enable CORS
+app = quart_cors.cors(Quart(__name__), allow_origin="*")
+
+TODOS = {}
+
+# Get authentication key from environment variable
+SERVICE_AUTH_KEY = os.environ.get("SERVICE_AUTH_KEY")
+
+# Add a before_request hook to check for authorization header
+@app.before_request
+def assert_auth_header():
+  auth_header = request.headers.get("Authorization")
+  print(auth_header)
+  # check if the header is missing or incorrect, and return an error if needed
+  if not auth_header or auth_header != f"Bearer {SERVICE_AUTH_KEY}":
+        return jsonify({"error": "Unauthorized"}), 401
+
+# Add a route to get all todos
+@app.route("/todos", methods=["GET"])
+async def get_todos():
+  return jsonify(TODOS)
+
+# Add a route to get all todos for a specific user
+@app.route("/todos/<string:username>", methods=["GET"])
+async def get_todo_user(username):
+    todos = TODOS.get(username, [])
+    return jsonify(todos)
+
+# Add a route to add a todo for a specific user
+@app.route("/todos/<string:username>", methods=["POST"])
+async def add_todo(username):
+    request_data = await request.get_json()
+    todo = request_data.get("todo", "")
+    TODOS.setdefault(username, []).append(todo)
+    return jsonify({"status": "success"})
+
+# Add a route to delete a todo for a specific user
+@app.route("/todos/<string:username>", methods=["DELETE"])
+async def delete_todo(username):
+    request_data = await request.get_json()
+    todo_idx = request_data.get("todo_idx", -1)
+    if 0 <= todo_idx < len(TODOS.get(username, [])):
+        TODOS[username].pop(todo_idx)
+    return jsonify({"status": "success"})
+
+
+@app.get("/logo.png")
+async def plugin_logo():
+  filename = 'logo.png'
+  return await quart.send_file(filename, mimetype='image/png')
+
+
+@app.get("/.well-known/ai-plugin.json")
+async def plugin_manifest():
+  host = request.headers['Host']
+  with open("manifest.json") as f:
+    text = f.read()
+    text = text.replace("PLUGIN_HOSTNAME", f"https://{host}")
+    return quart.Response(text, mimetype="text/json")
+
+
+@app.get("/openapi.yaml")
+async def openapi_spec():
+  host = request.headers['Host']
+  with open("openapi.yaml") as f:
+    text = f.read()
+    text = text.replace("PLUGIN_HOSTNAME", f"https://{host}")
+    return quart.Response(text, mimetype="text/yaml")
+
+
+def main():
+  app.run(debug=True, host="0.0.0.0", port=5002)
+
+
+if __name__ == "__main__":
+  main()
